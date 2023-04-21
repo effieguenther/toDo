f9b6cca4bfe89eb803464143fce8b2a01b345ea9
effieguenther <effieguenther@macbook-pro.myfiosgateway.com>
2023-04-20 23:02:49 -0400

    wip

--- toDo.js
+++ toDo.js
@@ -1,7 +1,22 @@
 function addTask() {
-    const newTask = document.createTextNode(document.querySelector('#task').value);
+    const newTask = document.querySelector('#task').value;
+    const newDeadline = document.querySelector('#deadline').value;
     const newLi = document.createElement('li');
+    const completeButton = document.createElement('button');
     const parentList = document.querySelector('#activeTasks');
-    newLi.appendChild(newTask);
+    
+
+    if (newDeadline) {
+        task = document.createTextNode(newTask + ' - ' + newDeadline);
+    } else {
+        task = document.createTextNode(newTask);
+    }
+
+    newLi.appendChild(task);
     parentList.appendChild(newLi);
+    
+}
+
+function completeTask() {
+    
 }
